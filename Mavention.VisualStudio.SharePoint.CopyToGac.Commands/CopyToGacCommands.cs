2b7e57ac32f7be60ac5b42138c352af9292a6bb2
Albert-Jan Schot <appie@digiwijs.nl>
2018-06-25 11:23:38 +0200

    Initial commit

---
+++ Mavention.VisualStudio.SharePoint.CopyToGac.Commands/CopyToGacCommands.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint;
+using Microsoft.VisualStudio.SharePoint.Commands;
+
+namespace Mavention.VisualStudio.SharePoint.CopyToGac.Commands {
+    public class CopyToGacCommands {
+        [SharePointCommand(CommandIds.GetApplicationPoolName)]
+        private string GetApplicationPoolName(ISharePointCommandContext context, string url) {
+            string applicationPoolName = null;
+
+            try {
+                using (SPSite site = new SPSite(url)) {
+                    applicationPoolName = site.WebApplication.ApplicationPool.Name;
+                }
+            }
+            catch {
+            }
+
+            return applicationPoolName;
+        }
+    }
+}
---
+++ Mavention.VisualStudio.SharePoint.CopyToGac/ThreadSafeStreamReader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mavention.VisualStudio.SharePoint.CopyToGac {
+    public class ThreadSafeStreamReader {
+        StreamReader streamReader = null;
+        string _Text = null;
+        public string Text {
+            get {
+                return _Text;
+            }
+        }
+
+        public ThreadSafeStreamReader(StreamReader sr) {
+            streamReader = sr;
+        }
+
+        public void Go() {
+            _Text = streamReader.ReadToEnd();
+        }
+    }
+}
